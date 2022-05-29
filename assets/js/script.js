ab38c8a8ae521af4d5fdebfebcac07d1b1dfe8b8
Ira Strubhar <taylor.strubhar@gmail.com>
2022-05-29 07:57:42 -0700

    initial commit, working through html and css

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// start button that begins a timer and presents first question
+    // timer runs in header, is affected by incorrect answers but persists between questions
+
+// answering a question leads to another question
+    // array of answers for each question
+
+// incorrect answers deduct time from the timer
+
+// when all questions are answered or the timer reaches 0 the game ends
+    // prompt alert for initial entry and save score to this entry in local storage
+
+// when the game is over, initials can be entered and saved to the high scores page
+    // use local storage to save scores between sessions
+    // submitting initials links to high score page
